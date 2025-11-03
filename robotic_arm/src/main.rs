67d102bf584201339c35464fd58293ba6a8c028c
Ahmed Alyafai <ahmedalyafai85@gmail.com>
2025-11-04 04:23:12 +0800

    Saving notes

--- robotic_arm/src/main.rs
+++ robotic_arm/src/main.rs
@@ -1,38 +1,70 @@
+use rand::prelude::*;
+use float_eq::float_eq;
+use std::thread;
+use std::time::Duration;
 use advanced_pid::{prelude::*, PidGain, Pid};
-// use pid::Pid;
-fn main() {
+#[derive(Debug)]
+struct Actual{ 
+    force: f32,
+    velocity: f32,
+    position: f32,
+}
+impl  Actual{
+
+    fn new() -> Self{
+        Self {
+            force: rand::random::<f32>(), 
+            velocity: rand::random::<f32>(),
+            position: rand::random::<f32>(),
+        }
+    }
+} 
+
+
+struct Target{ 
+    force: f32,
+    velocity: f32,
+    position: f32,
+}
+impl  Target{
+
+    fn new() -> Self{
+        Self {
+            force: rand::random::<f32>(), 
+            velocity: rand::random::<f32>(),
+            position: rand::random::<f32>(),
+        }
+    }
+} 
+
+fn calculate_pid(actual: &mut f32, target: &mut f32){   
     let gain = PidGain {
         kp: 1.0,
         ki: 0.8,
         kd: 0.1,
     };
     let mut pid = Pid::new(gain.into());
-
-    let target = 43.42;
-    let mut actual = 2.0;
     let dt = 0.1;
     loop {
         // Calculate control output
-        let output = pid.update(target, actual, dt);
+        let output = pid.update(*target, *actual, dt);
 
-        // Simulate the system response
-        actual += (output - actual) / 4.0;
-        println!("{:5.2}", actual);
+        // Simulate the system respons
+        *actual += (output - *actual) / 4.0;
+        println!("Current Readings Measurement: {actual}, : {target}");
         // Sleep 100ms
-    
-        std::thread::sleep(std::time::Duration::from_millis(20));
+
+        if float_eq!(actual, target, abs<= 0.00000_1){
+           println!() 
+        }
+        thread::sleep(Duration::from_millis(100));
     }
-    // println!("PID setpoint is : {:?}", pid.setpoint);
-    // println!("PID limit is : {:?}", pid.p_limit);
-    // // Test simple proportional
-    // let value=pid.next_control_output(0.0);
-    // let output= value.output - pid.setpoint;
-    // println!("{:?}, output after deducting error: {:?}", value, output);
-    // // Test proportional limit
-    // let value_1=pid.next_control_output(10.0);
-    // let output= value_1.output - pid.setpoint;
-    // println!("{:?}, output after deducting error: {:?}", value_1, output);
-    // let value_1=pid.next_control_output(30.0);
-    // let output= value_1.output - pid.setpoint;
-    // println!("{:?}, output after deducting error: {:?}", value_1, output);
 }
+fn main() {
+    let mut sensor_data=Actual::new(); 
+    let mut target_data=Target::new();
+    calculate_pid(&mut sensor_data.force,&mut target_data.force);
+    
+}
+
+
