2da8e729cd3961af4943fc47b807b691e2d67ba9
Ahmed Alyafai <ahmedalyafai85@gmail.com>
2025-11-10 14:44:24 +0800

    Update gitignore

--- robotic_arm/src/source-lib/target/debug/deps/source_lib-0cbaaaed7c841f32.d
+++
@@ -1,7 +0,0 @@
-/home/ahmed/MyOwnProjects/real-time-system-rust/robotic_arm/src/source-lib/target/debug/deps/source_lib-0cbaaaed7c841f32.d: src/lib.rs src/sensor/mod.rs src/actuator/mod.rs
-
-/home/ahmed/MyOwnProjects/real-time-system-rust/robotic_arm/src/source-lib/target/debug/deps/source_lib-0cbaaaed7c841f32: src/lib.rs src/sensor/mod.rs src/actuator/mod.rs
-
-src/lib.rs:
-src/sensor/mod.rs:
-src/actuator/mod.rs:
