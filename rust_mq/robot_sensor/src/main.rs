a36b1daaab18c31051276358c8a58e1d318136c2
1nuc <ahmedalyafai85@gmail.com>
2025-12-28 18:35:36 +0800

    Save notes

--- rust_mq/robot_sensor/src/main.rs
+++ rust_mq/robot_sensor/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use lapin::*;
+use tokio::{time::sleep, *};
+use std::time::Duration;
+
+async fn create_connection()-> Connection{
+    let addr="amqp://guest:guest@localhost:5672";
+    let mut res=Connection::connect(addr,ConnectionProperties::default()).await;
+    while res.is_err(){
+        println!("Failure in Connecting");
+        res=Connection::connect(addr,ConnectionProperties::default()).await;
+        sleep(Duration::from_secs(1)).await;
+    }
+    println!("Successful connection");
+    let connection= res.unwrap();
+    connection
+    
+}
+
+#[tokio::main]
+async fn main() {
+    
+    
+    let connection: Connection= create_connection().await;
+    //create a communication channel
+    let channel=connection.create_channel().await.expect("error in creating a channel");
+
 }
