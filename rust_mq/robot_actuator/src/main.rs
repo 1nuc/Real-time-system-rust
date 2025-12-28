4fb11a0b1b23acf635bd16b715ffb9d0809e7016
1nuc <ahmedalyafai85@gmail.com>
2025-12-28 23:47:16 +0800

    Save data

--- rust_mq/robot_actuator/src/main.rs
+++ rust_mq/robot_actuator/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{*, time::sleep};
+use futures_lite::stream::StreamExt;
+use lapin::{types::FieldTable, *, options::*};
+
+use std::time::Duration;
+async fn create_connection()-> Connection{
+    let addr="amqp://guest:guest@localhost:5672";
+    let mut res=Connection::connect(addr,ConnectionProperties::default()).await;
+    while res.is_err(){
+        println!("Failure in Connecting");
+        res=Connection::connect(addr,ConnectionProperties::default()).await;
+        sleep(Duration::from_secs(1)).await;
+    }
+    println!("Successful connection");
+    let connection= res.unwrap();
+    connection
+    
+}
+#[tokio::main]
+async fn main() {
+    let connection=create_connection().await;
+    let channel=connection.create_channel().await.expect("error in creating a channel");
+    let mut consumer= channel.basic_consume("sensing_data", "Actuator", BasicConsumeOptions::default(), FieldTable::default()).await;
+    while consumer.is_err(){
+         println!("Waiting for a message to recieve");
+         consumer= channel.basic_consume("sensing_data", "consumer", BasicConsumeOptions::default(), FieldTable::default()).await;
+         sleep(Duration::from_secs(2)).await;
+    }
+    while let Some(msg)=consumer.clone().unwrap().next().await{
+        if let Ok(msg)=msg{
+            println!("Message recieved, {:?}", msg.data);
+            msg.acker.ack(BasicAckOptions::default()).await;
+        }
+    }
+    
 }
