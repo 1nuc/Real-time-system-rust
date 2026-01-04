704f15a818af5f3669dc3c805df6927f56bcd421
1nuc <ahmedalyafai85@gmail.com>
2026-01-04 23:06:57 +0800

    Save updaets

--- Main/benches/sync_performance.rs
+++ Main/benches/sync_performance.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use manufacturer::{Actions, sensing_data::Readings};
-
+use sync_lib::{ControlSync, SensingSync, sensor, transmission_control};
+use std::sync::{Mutex, AtomicI32, Arc};
 fn measure_generation(c: &mut Criterion){
     let mut group= c.benchmark_group("Readings Generation Measurement");
     let scale=vec![10, 20, 30, 40, 50, 100];
@@ -27,10 +28,27 @@ fn measure_processing(c: &mut Criterion){
     }
 }
 
-fn data_transmission(c: Criterion){
-
+fn execute_tramission(size : i32){
+    let data=Readings::assign_data(size).filter_noise();
+    let channel=transmission_control::TransmissionChannel::init();
+    let counts=Arc::new(AtomicI32::new(data.objects_num.clone()));
+    let sensing_info=Arc::new(Mutex::new((data.current_state, data.objects.clone())));
+    data.sensor_control(sensing_info, channel.txes, channel.rxes, counts);
 }
 
-criterion_group!(benches,measure_generation, measure_processing);
+fn data_transmission(c: &mut Criterion){
+    let mut group= c.benchmark_group("Data Transmission");
+    let scale=vec![10, 20, 30, 40, 50, 100];
+    for size in scale.into_iter(){
+        group.bench_function(format!("data transmission, {:?} size",size), move |x|{
+            x.iter(||{
+                execute_tramission(size);
+            });
+        });
+
+    }
+}
+
+criterion_group!(benches,measure_generation, measure_processing, data_transmission);
 criterion_main!(benches);
 
