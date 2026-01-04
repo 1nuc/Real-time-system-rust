c2fa3d590d037356d68a7c9415c7d86024cf6e47
1nuc <ahmedalyafai85@gmail.com>
2026-01-05 02:30:52 +0800

    Add different sizes comparison between async and sync

--- Main/benches/performance_benchmark.rs
+++ Main/benches/performance_benchmark.rs
@@ -15,15 +15,18 @@ fn sync_benchmarker(size: i32){
 
 fn compare_async_sync(c: &mut Criterion){
     let mut group=c.benchmark_group("Async Vs Sync");
-    let size=50;
-    let runtime=Runtime::new().expect("unable to create tokio runtime"); 
-    group.bench_function("async version using tokio", |x|x.to_async(&runtime).iter(||async{
-                async_benchmarker(size).await;
-        }),
-    );
-    group.bench_function("sync version using rust multi thread environment", |x|x.iter(||{
-        sync_benchmarker(size);
-    }));
+    let sizes=vec![10, 30, 50, 100];
+    for size in sizes.into_iter(){
+        let runtime=Runtime::new().expect("unable to create tokio runtime"); 
+        group.bench_function(format!("async version using tokio for size {:?}", size), |x|x.to_async(&runtime).iter(||async{
+                    async_benchmarker(size).await;
+            }),
+        );
+        group.bench_function(format!("sync version using rust multi thread environment for size {:?}", size), |x|x.iter(||{
+            sync_benchmarker(size);
+        }));
+
+    }
 }
 criterion_group!(benches, compare_async_sync);
 criterion_main!(benches);
--- Main/src/main.rs
+++ Main/src/main.rs
@@ -1,26 +1,12 @@
 // use sync_lib::*;
-// use async_lib::*;
-// use tokioo;
-use tokio::{process::Command, try_join};
-use std::path::Path;
+use async_lib::*;
+use tokio;
 #[tokio::main]
 async fn main() {
         // let simulation=transmission_control::TransmissionChannel::init();
         // simulation.simulation_control(32);
-        // let simulation=transmission_control::TransmissionChannel::init();
-        // simulation.simulation_control(30).await;
-    // let _delete_server_cache=Command::new("rm").args(&["-r", "../../rust_mq/robot_actuator/target/"]).output().await; 
-    // let _delete_client_cache=Command::new("rm").args(&["-r", "../../rust_mq/robot_sensor/target/"]).output().await; 
-    //
-    // let _build_server=Command::new("cargo").args(&["b", "--manifest-path", "../../rust_mq/robot_sensor/Cargo.toml"]).output().await; 
-    // let _build_client=Command::new("cargo").args(&["b", "--manifest-path", "../../rust_mq/robot_actuator/Cargo.toml"]).output().await; 
-
-    let client="../../rust_mq/robot_actuator/target/debug/robot_actuator";
-    let server="../../rust_mq/robot_sensor/target/debug/robot_sensor";
-    let  mut server_t=Command::new(server).spawn().expect("error in server");
-    let  mut client_t=Command::new(client).spawn().expect("error in the client"); 
-
-    let _=try_join!(server_t.wait(), client_t.wait());
+        let simulation=transmission_control::TransmissionChannel::init();
+        simulation.simulation_control(30).await;
 
 }
 
